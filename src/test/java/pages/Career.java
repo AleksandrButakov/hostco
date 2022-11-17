502d11b3583714d74eb335fb7908e7b7766d929b
Aleksandr Butakov <anbngm@gmail.com>
2022-11-17 23:07:46 +0400

    1

--- src/test/java/pages/Career.java
+++ src/test/java/pages/Career.java
@@ -4,7 +4,6 @@ import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Selenide.$$;
-
 public class Career {
 
     private SelenideElement
